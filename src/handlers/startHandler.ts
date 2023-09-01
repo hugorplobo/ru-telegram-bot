a13e45d7f18d56913705b57a7bd19afe51d05492
Antônio Hugo Ribeiro Pereira Lobo <hugorplobo@gmail.com>
2023-09-01 01:19:33 -0300

    feat: update start message

--- src/handlers/startHandler.ts
+++ src/handlers/startHandler.ts
@@ -2,11 +2,13 @@ import { Context } from "grammy";
 
 export async function startHandler(ctx: Context) {
   await ctx.reply(`
-    /cadastrar <numero do cartão> <matrícula> - Cadastre seu cartão para consultas\n
-    /descadastrar - Remova seu cartão do banco de dados\n
-    /info - Verifique quantos créditos você possui\n
-    /cardapio - Receba o cardápio do dia\n
-    /feedback <mensagem> - Envie uma mensagem ao desenvolvedor, o seu nome de usuário também é compartilhado!\n\n
-    \nEm breve envio automático do cardápio e créditos diariamente!
-  `.replace(/^[\s\t]+/gm, ""));
+    /start ou /ajuda - Envia essa mensagem
+    /cadastrar <numero do cartão> <matrícula> - Cadastre seu cartão para consultas
+    /descadastrar - Remova seu cartão do banco de dados
+    /info - Verifique quantos créditos você possui
+    /cardapio - Receba o cardápio do dia
+    /feedback <mensagem> - Envie uma mensagem ao desenvolvedor, o seu nome de usuário também é compartilhado!
+    
+    Em breve envio automático do cardápio e créditos diariamente!
+  `.replace(/^ +/gm, ""));
 }
