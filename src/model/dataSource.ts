5ac49c9c26c51764290d45290b0b96d5836eb943
Antônio Hugo Ribeiro Pereira Lobo <hugorplobo@gmail.com>
2023-09-04 19:46:25 -0300

    feat: add subscriber model

--- src/model/dataSource.ts
+++ src/model/dataSource.ts
@@ -1,9 +1,10 @@
 import { DataSource } from "typeorm";
 import { User } from "./user";
+import { Subscriber } from "./subscriber";
 
 export const AppDataSource = new DataSource({
   type: "sqlite",
   database: process.env.DATABASE_FILE!,
   synchronize: true,
-  entities: [ User ],
+  entities: [ User, Subscriber ],
 });
