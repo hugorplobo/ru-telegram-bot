79e5ab327e31c4e0b6fc1f6be552f930984860d0
Antônio Hugo Ribeiro Pereira Lobo <hugorplobo@gmail.com>
2024-07-23 10:53:42 -0300

    feat: dont forward message starting with slash

--- src/handlers/forwardHandler.ts
+++ src/handlers/forwardHandler.ts
@@ -3,6 +3,10 @@ import { AppDataSource } from "../model/dataSource";
 import { User } from "../model/user";
 
 export async function forwardHandler(ctx: Context) {
+  if (ctx.message?.text !== undefined && ctx.message.text.startsWith("/")) {
+    return;
+  }
+
   const users = await AppDataSource.getRepository(User).find();
 
   for (const user of users) {
